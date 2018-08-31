c1a19eb63c7774fe3b83d4c7a6253aba674faa67
wangfei <gaodun123com>
2018-08-31 14:53:55 +0800

    Initial commit

---
+++ src/main/java/com/kuanquan/springboot/SpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.kuanquan.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 用于启动程序的类
+ */
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+}
