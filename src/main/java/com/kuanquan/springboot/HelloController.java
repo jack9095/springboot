20c4e748c441dd0f75a9545467904fca326005da
wangfei <gaodun123com>
2018-10-30 09:52:01 +0800

    加入mybatis

--- src/main/java/com/kuanquan/springboot/HelloController.java
+++ src/main/java/com/kuanquan/springboot/HelloController.java
@@ -11,7 +11,12 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello(){
-        return "hello spring boot fly 热部署 ";
+        return "hello spring boot fly 热部署\n " +
+                "元日   \n " +
+                "爆竹声中一岁除，\n " +
+                "春风送暖入屠苏，  \n  " +
+                "千门万户曈曈日，  \n  " +
+                "总把新桃换旧符 ";
     }
 
 }
