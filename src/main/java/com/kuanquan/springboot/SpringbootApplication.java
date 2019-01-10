35e5f24ad420e4aac8ca7ef677fa2ff3fb785643
Administrator <929728742@qq.com>
2019-01-10 23:24:14 +0800

    解决一个小bug

--- src/main/java/com/kuanquan/springboot/SpringbootApplication.java
+++ src/main/java/com/kuanquan/springboot/SpringbootApplication.java
@@ -2,11 +2,13 @@ package com.kuanquan.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 用于启动程序的类
+ * bug解决：https://blog.csdn.net/coyotess/article/details/80637837
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
