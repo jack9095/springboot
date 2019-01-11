ed8a97f67724a355fcf8f147a59e7bf9bc8783d5
wangfei <gaodun123com>
2019-01-11 18:20:18 +0800

    加入配置

--- src/main/java/com/kuanquan/springboot/SpringbootApplication.java
+++ src/main/java/com/kuanquan/springboot/SpringbootApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * 用于启动程序的类
  * bug解决：https://blog.csdn.net/coyotess/article/details/80637837
  */
+// 是 Spring Boot 的核心注解，它是一个组合注解，该注解组合了：@Configuration、@EnableAutoConfiguration、@ComponentScan；
+// 若不是用 @SpringBootApplication 注解也可以使用这三个注解代替
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SpringbootApplication {
 
